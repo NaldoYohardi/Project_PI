5578440e59f1a9b3edf270a7ad18613097f824f7
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-05-26 10:10:15 +0700

    bug fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,31 +4,6 @@
 @section('MainTitle', 'Register')
 
 @section('content')
-<<<<<<< HEAD
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="limiter">
-                <div class="container-login100">
-                    <form method="POST" action="{{ route('register') }}" class="login100-form validate-form">
-                        @csrf
-                        <span class="login100-form-title">
-                        {{ __('REGISTER') }}
-                        </span>
-
-                        <div class="wrap-input100 validate-input" data-validate = "Valid name is required.">
-                          <input id="name" class="input100 form-control @error('name') is-invalid @enderror" type="text" name="name" placeholder="{{ __('Username') }}" value="{{ old('name') }}" autofocus>
-                          @error('name')
-                              <span class="invalid-feedback" role="alert">
-                                  <strong>{{ $message }}</strong>
-                              </span>
-                          @enderror
-                          <span class="focus-input100"></span>
-                          <span class="symbol-input100">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-=======
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   @foreach (['danger', 'warning', 'success', 'info'] as $msg)
     @if(Session::has('alert-' . $msg))
@@ -56,7 +31,6 @@
                       @error('name')
                           <span class="invalid-feedback" role="alert">
                               <strong>{{ $message }}</strong>
->>>>>>> ab03a75ed80fb0d45d054852361637a74e7c9e7c
                           </span>
                       @enderror
                     </div>
@@ -66,54 +40,6 @@
                           <span class="invalid-feedback" role="alert">
                               <strong>{{ $message }}</strong>
                           </span>
-<<<<<<< HEAD
-                        </div>
-
-                        <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                          <input id="password" class="input100 form-control @error('password') is-invalid @enderror" value="asd" type="text" name="password" placeholder="{{ __('Password') }}">
-                          @error('password')
-                              <span class="invalid-feedback" role="alert">
-                                  <strong>{{ $message }}</strong>
-                              </span>
-                          @enderror
-                          <span class="focus-input100"></span>
-                          <span class="symbol-input100">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                          </span>
-                        </div>
-
-                        <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                          <input id="password-confirm" class="input100 form-control @error('password-confirm') is-invalid @enderror" value="" type="password" name="password_confirmation" placeholder="{{ __('Password') }}">
-                          @error('password')
-                              <span class="invalid-feedback" role="alert" id="message">
-                                  <strong>{{ $message }}</strong>
-                              </span>
-                          @enderror
-                          <span class="focus-input100"></span>
-                          <span class="symbol-input100">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                          </span>
-                        </div>
-                        <p class="alert alert-danger" style="visibility: hidden; text-align: center;" id="demo"></p>
-                        <div class="container-login100-form-btn">
-                          <button type="submit" class="login100-form-btn" onclick="return myFunction()">
-                            Register
-                          </button>
-                        </div>
-                    </form>
-
-                    <script>
-                    function myFunction() {
-                      if(document.getElementById("password-confirm").value != document.getElementById("password").value){
-                        document.getElementById("demo").innerHTML = "Password Mismatch";
-                        document.getElementById("demo").style.visibility = "visible";
-                        return false;
-                      } else{
-                        document.getElementById("demo").style.visibility = "hidden";
-                      }
-                    }
-                    </script>
-=======
                       @enderror
                     </div>
                     <div class="form-group">
@@ -158,4 +84,3 @@
       }
     }
 </script>
->>>>>>> ab03a75ed80fb0d45d054852361637a74e7c9e7c
