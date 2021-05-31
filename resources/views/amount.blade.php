b2c91b85bf635404cd74accc5cb329f4b194b332
aldrend <liongod452.aw@gmail.com>
2021-05-31 16:35:01 +0700

    Update FrontEnd Manager Level View

--- resources/views/amount.blade.php
+++ resources/views/amount.blade.php
@@ -1,13 +1,26 @@
-<form action="/addData" method="post">
-    @csrf
-    <div class="form-group">
-      <label for="sel1">Select list (select one):</label>
-      <select name="amount" class="form-control" id="sel1">
-        <?php for ($i=1; $i <=10 ; $i++) {?>
-        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-        <?php } ?>
-      </select>
-      <br>
-      <input type="submit" name="submit" value="submit">
+@extends('layouts.app')
+
+@section('title', 'Add Amount')
+@section('MainTitle', 'Add Amount')
+
+@section('content')
+  <div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <form action="/addData" method="post">
+          @csrf
+          <div class="form-group">
+            <label for="sel1">Select list (select one):</label>
+            <select name="amount" class="form-control" id="sel1">
+              <?php for ($i=1; $i <=10 ; $i++) {?>
+              <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+              <?php } ?>
+            </select>
+            <br>
+            <input type="submit" name="submit" value="submit">
+          </div>
+        </form>
+      </div>
     </div>
-  </form>
+  </div>
+@endsection
--- resources/views/tambahData.blade.php
+++ resources/views/tambahData.blade.php
@@ -1,23 +1,36 @@
-<?php echo Session::get('email'); ?>
-<form action="/tambahData" method="post">
-  @csrf
-  <?php for ($i=0; $i <$n ; $i++) {?>
-    <h3>Data barang <?php echo $i+1; ?></h3>
-    <input type="hidden" name="user_id" value="<?php echo Session::get('user_id'); ?>">
-    <input type="hidden" name="n" value="<?php echo $n; ?>">
-    Nama <br>
-    <input class="form-control" type="text" name="name<?php echo $i; ?>"><br>
-    stok<br>
-    <input class="form-control" type="number" name="stok<?php echo $i; ?>"><br>
-    category<br>
-    <select name="category<?php echo $i; ?>">
-      @foreach($category as $key)
-      <option value="{{$key->id}}">{{$key->category}}</option>
-      @endforeach
-    </select><br>
-    Harga per Unit<br>
-    <input class="form-control" type="number" name="harga<?php echo $i; ?>"><br>
-    <br>
-  <?php  }?>
-  <input type="submit" value="submit">
-</form>
+@extends('layouts.app')
+
+@section('title', 'Add Item')
+@section('MainTitle', 'Add Item')
+
+@section('content')
+<div class="col-lg-12 grid-margin stretch-card">
+  <div class="card">
+    <div class="card-body">
+      <?php echo Session::get('email'); ?>
+      <form action="/tambahData" method="post">
+        @csrf
+        <?php for ($i=0; $i <$n ; $i++) {?>
+          <h3>Data barang <?php echo $i+1; ?></h3>
+          <input type="hidden" name="user_id" value="<?php echo Session::get('user_id'); ?>">
+          <input type="hidden" name="n" value="<?php echo $n; ?>">
+          Nama <br>
+          <input class="form-control" type="text" name="name<?php echo $i; ?>"><br>
+          stok<br>
+          <input class="form-control" type="number" name="stok<?php echo $i; ?>"><br>
+          category<br>
+          <select name="category<?php echo $i; ?>">
+            @foreach($category as $key)
+            <option value="{{$key->id}}">{{$key->category}}</option>
+            @endforeach
+          </select><br>
+          Harga per Unit<br>
+          <input class="form-control" type="number" name="harga<?php echo $i; ?>"><br>
+          <br>
+        <?php  }?>
+        <input type="submit" value="submit">
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
