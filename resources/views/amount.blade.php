dcb7274f0115c35821c942aa0fddbb63b0e2787f
Kevin Patrick Lee <kevin.patrick.lee.1@gmail.com>
2021-05-31 20:08:11 +0700

    Update View tambahData

--- resources/views/amount.blade.php
+++ resources/views/amount.blade.php
@@ -15,14 +15,14 @@
         <form action="/addData" method="post">
           @csrf
           <div class="form-group">
-            <label for="sel1">Select list (select one):</label>
+            <label for="sel1"><h4>Select list (select one):</h4></label>
             <select name="amount" class="form-control" id="sel1">
               <?php for ($i=1; $i <=10 ; $i++) {?>
               <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
               <?php } ?>
             </select>
             <br>
-            <input type="submit" class="btn-sm font-weight-bold btn-primary" name="submit" value="submit">
+            <input type="submit" class="btn-sm font-weight-bold btn-primary" name="submit" value="Submit">
           </div>
         </form>
       </div>
