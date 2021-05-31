8f64020421d4bb6d84031d461533b01b1b8ec298
aldrend <liongod452.aw@gmail.com>
2021-05-31 17:23:24 +0700

    Update FrontEnd Inventory Add View

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -16,36 +16,38 @@
         List containing Inventories from Database
       </p>
       <?php if(Session::get('level')== 0){ ?>
-        <a href="/add">Request Tambah data</a>
-        <table id="example" class="hover table table-bordered table-striped">
-          <thead class="thead-dark font-weight-bold text-center">
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Stok</th>
-              <th>Qr Code</th>
-              <th>Category</th>
-              <th>Harga_unit</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            @if($inventory != NULL)
-            @foreach($inventory as $key)
-            <tr>
-              <td>{{ $loop->iteration }}</td>
-              <td>{{ $key->name }}</td>
-              <td>{{ $key->stok }}</td>
-              <td>{{ $key->qr_code }}</td>
-              <td>{{ $key->harga_unit }}</td>
-              <td> <a href="#" class="btn-sm font-weight-bold btn-warning w-50">Edit</a> </td>
-              <td> <a href="#" class="btn-sm font-weight-bold btn-danger w-50">Delete</a> </td>
-            </tr>
-            @endforeach
-            @endif
-          </tbody>
-        </table>
+        <a href="/add" class="btn-sm font-weight-bold btn-success w-50">Request Tambah data</a>
+        <div class="card-body">
+          <table id="example" class="hover table table-bordered table-striped">
+            <thead class="thead-dark font-weight-bold text-center">
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Stok</th>
+                <th>Qr Code</th>
+                <th>Category</th>
+                <th>Harga_unit</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @if($inventory != NULL)
+              @foreach($inventory as $key)
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $key->name }}</td>
+                <td>{{ $key->stok }}</td>
+                <td>{{ $key->qr_code }}</td>
+                <td>{{ $key->harga_unit }}</td>
+                <td> <a href="#" class="btn-sm font-weight-bold btn-warning w-50">Edit</a> </td>
+                <td> <a href="#" class="btn-sm font-weight-bold btn-danger w-50">Delete</a> </td>
+              </tr>
+              @endforeach
+              @endif
+            </tbody>
+          </table>
+        </div>
       <?php } elseif(Session::get('level')==2){ ?>
         <a href="/add" class="btn-sm font-weight-bold btn-success w-50">Request Tambah data</a>
         <div class="card-body">
--- resources/views/tambahData.blade.php
+++ resources/views/tambahData.blade.php
@@ -10,33 +10,33 @@
     <li><a href="{{ url('/add')}}">Request Amount</a></li>
     <li>Add Item</li>
   </ul>
-  <div class="col-lg-12 grid-margin stretch-card">
-    <div class="card">
-      <div class="card-body">
         <?php echo Session::get('email'); ?>
         <form action="/tambahData" method="post">
           @csrf
-          <?php for ($i=0; $i <$n ; $i++) {?>
-            <h3>Data barang <?php echo $i+1; ?></h3>
-            <input type="hidden" name="user_id" value="<?php echo Session::get('user_id'); ?>">
-            <input type="hidden" name="n" value="<?php echo $n; ?>">
-            Nama <br>
-            <input class="form-control" type="text" name="name<?php echo $i; ?>"><br>
-            stok<br>
-            <input class="form-control" type="number" name="stok<?php echo $i; ?>"><br>
-            category<br>
-            <select name="category<?php echo $i; ?>">
-              @foreach($category as $key)
-              <option value="{{$key->id}}">{{$key->category}}</option>
-              @endforeach
-            </select><br>
-            Harga per Unit<br>
-            <input class="form-control" type="number" name="harga<?php echo $i; ?>"><br>
-            <br>
-          <?php  }?>
+            <?php for ($i=0; $i <$n ; $i++) {?>
+            <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h3>Data barang <?php echo $i+1; ?></h3>
+                  <input type="hidden" name="user_id" value="<?php echo Session::get('user_id'); ?>">
+                  <input type="hidden" name="n" value="<?php echo $n; ?>">
+                  Nama <br>
+                  <input class="form-control" type="text" name="name<?php echo $i; ?>"><br>
+                  stok<br>
+                  <input class="form-control" type="number" name="stok<?php echo $i; ?>"><br>
+                  category<br>
+                  <select name="category<?php echo $i; ?>">
+                    @foreach($category as $key)
+                    <option value="{{$key->id}}">{{$key->category}}</option>
+                    @endforeach
+                  </select><br>
+                  Harga per Unit<br>
+                  <input class="form-control" type="number" name="harga<?php echo $i; ?>"><br>
+                  <br>
+                </div>
+              </div>
+            </div>
+            <?php  }?>
+          </form>
           <input type="submit" class="btn-sm font-weight-bold btn-primary" value="submit">
-        </form>
-      </div>
-    </div>
-  </div>
 @endsection
