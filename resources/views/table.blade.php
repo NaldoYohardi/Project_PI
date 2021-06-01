7a52f042b2e7c3059cfacfa164f926a17468b158
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-06-01 14:00:52 +0700

    fix show qr code

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -101,6 +101,7 @@
                 }
               }
               $n = $j;
+              $a = array();
               for ($i=0; $i <=$j ; $i++) {
                 if(implode("",$status1[$i])=="3"){
                 ?>
@@ -114,13 +115,14 @@
                       <td align="center"><?php echo $key->category; ?></td>
                   <?php  }
                   } ?>
+                  <?php $a = implode("", $names[$i]); ?>
                   <td><center>
                     <img src="data:image/png;base64,
                               {!! base64_encode(
                                   QrCode::format('png')
                                   ->merge(public_path('laravel.PNG'), 0.3, true)
                                   ->size(100)
-                                  ->generate('{{implode("",$names)}}')
+                                  ->generate($a)
                                 )
                               !!}
                     ">
@@ -232,12 +234,13 @@
                   <?php  }
                   } ?>
                   <td><center>
+                    <?php $a = implode("", $names[$i]); ?>
                     <img src="data:image/png;base64,
                               {!! base64_encode(
                                   QrCode::format('png')
                                   ->merge(public_path('laravel.PNG'), 0.3, true)
                                   ->size(100)
-                                  ->generate('{{implode("",$names)}}')
+                                  ->generate($a)
                                 )
                               !!}
                     ">
