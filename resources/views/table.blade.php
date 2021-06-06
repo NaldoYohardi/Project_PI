af0f06759565b497dafa04c4cc9aa662605529a2
Endity <enditywasita@gmailcom>
2021-06-06 16:31:57 +0700

    a

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -23,12 +23,81 @@
             <tr>
               <th>ID</th>
               <th>User Request</th>
-              <th>Item Name</th>
+              <th>Name</th>
               <th>Stock</th>
               <th>Category</th>
-              <th>QR</th>
-              <th>Unit Price</th>
-              <th>Checked By</th>
+              <th>Qr Code</th>
+              <th>Harga unit</th>
+              <th>User Approve</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody class="break" align="center">
+            @foreach ($inbox as $key)
+            <tr>
+              <td>{{ $loop->iteration }}</td>
+              @foreach($user as $key1)
+              @if($key1->user_id == $key->req_id)
+              <td>{{ $key1->name }}</td>
+              @endif
+              @endforeach
+              <td>{{ $key->name }}</td>
+              <td>{{ $key->stok }}</td>
+              @foreach ($category as $key1)
+              @if ($key1->id == $key->category_id)
+              <td>{{ $key1->category }}</td>
+              @endif
+              @endforeach
+              <td><?php $a = $key->name; ?>
+                <a href="data:image/png;base64, {!!
+                            base64_encode(QrCode::format('png')
+                            ->merge(public_path('laravel.PNG'), 0.3, true)
+                            ->size(100)
+                            ->generate($a))
+                            !!}
+                            ">
+                <img src="data:image/png;base64, {!!
+                            base64_encode(QrCode::format('png')
+                            ->merge(public_path('laravel.PNG'), 0.3, true)
+                            ->size(100)
+                            ->generate($a))
+                            !!}
+                            "></a></td>
+              <td>Rp.{{ $key->harga_unit }}</td>
+              @if ($key->approval_id == NULL)
+              <td>-</td>
+              @else
+                @foreach($user as $key1)
+                @if($key1->user_id == $key->approval_id)
+                <td>{{ $key1->name }}</td>
+                @endif
+                @endforeach
+              @endif
+              <td class="fixbreak"><center>
+                <a href="/addstok/<?php echo $key->id; ?>" class="inv-btn inv-success">Input</a><br>
+                <a href="/outstok/<?php echo $key->id; ?>" class="inv-btn inv-info">Output</a><br>
+                <a href="/editInventory/<?php echo $key->id; ?>" class="inv-btn inv-primary">Edit</a><br>
+                <a href="/deleteInventory/<?php echo $key->id; ?>" class="inv-btn inv-danger" onclick="return confirm('Confirm Item Deletion?');">Delete</a>
+              </center></td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      <?php } ?>
+      <?php if(Session::get('level')== 0){ ?>
+        <a href="/add" class="btn-sm font-weight-bold btn-success w-50">Add Request</a>
+        <br></br>
+        <table id="example" class="hover table table-bordered table-striped">
+          <thead class="thead-dark font-weight-bold text-center">
+            <tr>
+              <th>ID</th>
+              <th>User Request</th>
+              <th>Name</th>
+              <th>Stock</th>
+              <th>Category</th>
+              <th>Qr Code</th>
+              <th>Harga unit</th>
+              <th>User Approve</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -67,68 +136,15 @@
               @if ($key->approval_id == NULL)
               <td>-</td>
               @else
-                @foreach($user as $key1)
-                @if($key1->user_id == $key->approval_id)
-                <td>{{ $key1->name }}</td>
-                @endif
-                @endforeach
+              @foreach($user as $key1)
+              @if($key1->user_id == $key->approval_id)
+              <td>{{ $key1->name }}</td>
+              @endif
+              @endforeach
               @endif
               <td class="fixbreak"><center>
                 <a href="/addstok/<?php echo $key->id; ?>" class="inv-btn inv-success">Input</a><br>
-                <a href="/outstok/<?php echo $key->id; ?>" class="inv-btn inv-info">Output</a><br>
-                <a href="/editInventory/<?php echo $key->id; ?>" class="inv-btn inv-primary">Edit</a><br>
-                <a href="/deleteInventory/<?php echo $key->id; ?>" class="inv-btn inv-danger" onclick="return confirm('Confirm Item Deletion?');">Delete</a>
-              </center></td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-      <?php } ?>
-      <?php if(Session::get('level')== 0){ ?>
-        <a href="/add" class="btn-sm font-weight-bold btn-success w-50">Add Request</a>
-        <br></br>
-        <table id="example" class="hover table table-bordered table-striped">
-          <thead class="thead-dark font-weight-bold text-center">
-            <tr>
-              <th>ID</th>
-              <th>Item Name</th>
-              <th>Stock</th>
-              <th>Category</th>
-              <th>QR</th>
-              <th>Unit Price</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody class="break" align="center">
-            @foreach ($inbox as $key)
-            <tr>
-              <td>{{ $loop->iteration }}</td>
-              <td>{{ $key->name }}</td>
-              <td>{{ $key->stok }}</td>
-              @foreach ($category as $key1)
-              @if ($key1->id == $key->category_id)
-              <td>{{ $key1->category }}</td>
-              @endif
-              @endforeach
-              <td><?php $a = $key->name; ?>
-                <a href="data:image/png;base64, {!!
-                            base64_encode(QrCode::format('png')
-                            ->merge(public_path('laravel.PNG'), 0.3, true)
-                            ->size(100)
-                            ->generate($a))
-                            !!}
-                            ">
-                <img src="data:image/png;base64, {!!
-                            base64_encode(QrCode::format('png')
-                            ->merge(public_path('laravel.PNG'), 0.3, true)
-                            ->size(100)
-                            ->generate($a))
-                            !!}
-                            "></a></td>
-              <td class="fixbreak">Rp.{{ $key->harga_unit }}</td>
-              <td class="fixbreak"><center>
-                <a href="/addstok/<?php echo Session::get('user_id'); ?>" class="inv-btn inv-success">Input</a><br>
-                <a href="/outstok/<?php echo Session::get('user_id'); ?>" class="inv-btn inv-info">Output</a>
+                <a href="/outstok/<?php echo $key->id; ?>" class="inv-btn inv-info">Output</a>
               </center></td>
             </tr>
             @endforeach
